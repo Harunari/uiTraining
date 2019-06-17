e84ab48d14f1219d915e1176b23dec9a471f22da
haruanri <nanteneus@hotmail.co.jp>
2019-06-17 23:05:53 +0900

    implement constraining movement

--- node/app/public/javascripts/index.js
+++ node/app/public/javascripts/index.js
@@ -3,11 +3,53 @@ window.onload = () => { };
 function makeDraggable(evt) {
     new DraggableObject(evt.target);
 }
+class ConfinedOption {
+    constructor() {
+        this.isConfined = false;
+        this.minX = 0;
+        this.maxX = 0;
+        this.minY = 0;
+        this.maxY = 0;
+        this.boundaryX1 = 10.5;
+        this.boundaryX2 = 30;
+        this.boundaryY1 = 2.2;
+        this.boundaryY2 = 19.2;
+    }
+    calcRealm(svgGraphicElement) {
+        if (!this.isConfined) {
+            return;
+        }
+        let bbox = svgGraphicElement.getBBox();
+        this.minX = this.boundaryX1 - bbox.x;
+        this.maxX = this.boundaryX2 - bbox.x - bbox.width;
+        this.minY = this.boundaryY1 - bbox.y;
+        this.maxY = this.boundaryY2 - bbox.y - bbox.height;
+    }
+    getConstratedCoordinate(dx, dy) {
+        if (!this.isConfined) {
+            return { x: dx, y: dy };
+        }
+        if (dx < this.minX) {
+            dx = this.minX;
+        }
+        else if (dx > this.maxX) {
+            dx = this.maxX;
+        }
+        if (dy < this.minY) {
+            dy = this.minY;
+        }
+        else if (dy > this.maxY) {
+            dy = this.maxY;
+        }
+        return { x: dx, y: dy };
+    }
+}
 class DraggableObject {
     constructor(svgTarget) {
         this.selectedElement = null;
         this.offset = { x: 0, y: 0 };
         this.transform = null;
+        this.confinedOption = new ConfinedOption();
         this.rootSvgElement = svgTarget;
         svgTarget.addEventListener('mousedown', e => this.startDrag(e));
         svgTarget.addEventListener('mousemove', e => this.drag(e));
@@ -24,6 +66,8 @@ class DraggableObject {
             this.selectedElement = eTarget.parentNode;
             this.offset = this.initializeDragging(event);
         }
+        this.confinedOption.isConfined = eTarget.classList.contains('confine');
+        this.confinedOption.calcRealm(eTarget);
     }
     drag(event) {
         if (!this.rootSvgElement) {
@@ -37,7 +81,11 @@ class DraggableObject {
         }
         event.preventDefault();
         const coordinate = this.getMousePostion(event);
-        this.transform.setTranslate(coordinate.x - this.offset.x, coordinate.y - this.offset.y);
+        const dx = coordinate.x - this.offset.x;
+        const dy = coordinate.y - this.offset.y;
+        const fixedCoordinate = this.confinedOption.getConstratedCoordinate(dx, dy);
+        this.transform.setTranslate(fixedCoordinate.x, fixedCoordinate.y);
+        console.log(fixedCoordinate.y);
     }
     endDrag() {
         this.selectedElement = null;
--- node/app/public/typescripts/index.ts
+++ node/app/public/typescripts/index.ts
@@ -4,17 +4,57 @@ function makeDraggable(evt: Event) {
   new DraggableObject(<SVGSVGElement>evt.target);
 }
 
-interface ICoordinate {
+interface Coordinate {
   x: number;
   y: number;
+}
+
+class ConfinedOption {
+  public isConfined: boolean = false;
+  public minX: number = 0;
+  public maxX: number = 0;
+  public minY: number = 0;
+  public maxY: number = 0;
+
+  public readonly boundaryX1: number = 10.5;
+  public readonly boundaryX2: number = 30;
+  public readonly boundaryY1: number = 2.2;
+  public readonly boundaryY2: number = 19.2;
+
+  public calcRealm(svgGraphicElement: SVGGraphicsElement): void {
+    if (!this.isConfined) { return; }
+
+    let bbox = svgGraphicElement.getBBox();
+    this.minX = this.boundaryX1 - bbox.x;
+    this.maxX = this.boundaryX2 - bbox.x - bbox.width;
+    this.minY = this.boundaryY1 - bbox.y;
+    this.maxY = this.boundaryY2 - bbox.y - bbox.height;
+  }
+
+  public getConstratedCoordinate(dx: number, dy: number): Coordinate {
+    if (!this.isConfined) { return { x: dx, y: dy }; }
+
+    if (dx < this.minX) {
+      dx = this.minX;
+    } else if (dx > this.maxX) {
+      dx = this.maxX;
+    }
+
+    if (dy < this.minY) {
+      dy = this.minY
+    } else if (dy > this.maxY) {
+      dy = this.maxY;
+    }
+    return { x: dx, y: dy };
+  }
 }
 
 class DraggableObject {
   private rootSvgElement: SVGSVGElement;
   private selectedElement: SVGSVGElement | null = null;
-  private offset: ICoordinate = { x: 0, y: 0 };
+  private offset: Coordinate = { x: 0, y: 0 };
   private transform: SVGTransform | null = null;
-
+  private confinedOption = new ConfinedOption();
 
   constructor(svgTarget: SVGSVGElement) {
     this.rootSvgElement = svgTarget;
@@ -33,6 +73,9 @@ class DraggableObject {
       this.selectedElement = <SVGSVGElement>eTarget.parentNode;
       this.offset = this.initializeDragging(event);
     }
+
+    this.confinedOption.isConfined = eTarget.classList.contains('confine');
+    this.confinedOption.calcRealm(eTarget as SVGGraphicsElement);
   }
   private drag(event: Event): void {
     if (!this.rootSvgElement) { return; }
@@ -41,12 +84,16 @@ class DraggableObject {
 
     event.preventDefault();
     const coordinate = this.getMousePostion(<MouseEvent>event);
-    this.transform.setTranslate(coordinate.x - this.offset.x, coordinate.y - this.offset.y);
+    const dx = coordinate.x - this.offset.x;
+    const dy = coordinate.y - this.offset.y;
+    const fixedCoordinate = this.confinedOption.getConstratedCoordinate(dx, dy);
+    this.transform.setTranslate(fixedCoordinate.x, fixedCoordinate.y);
+    console.log(fixedCoordinate.y);
   }
   private endDrag(): void {
     this.selectedElement = null;
   }
-  private initializeDragging(event: Event): ICoordinate {
+  private initializeDragging(event: Event): Coordinate {
     if (!this.selectedElement) { return { x: 0, y: 0 }; }
     if (!this.rootSvgElement) { return { x: 0, y: 0 }; }
 
@@ -64,7 +111,7 @@ class DraggableObject {
     offset.y -= this.transform.matrix.f;
     return offset;
   }
-  private getMousePostion(event: MouseEvent): ICoordinate {
+  private getMousePostion(event: MouseEvent): Coordinate {
     const svgElement = <SVGGraphicsElement>this.rootSvgElement;
     const ctm = <DOMMatrix>svgElement.getScreenCTM();
     return {
